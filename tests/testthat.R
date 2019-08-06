11c8da3d4a0f430f0b5f7fa210bfaa071076a9bc
Pakillo <f.rodriguez.sanc@gmail.com>
2019-08-06 18:08:42 +0200

    use tests

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(safeinstall)
+
+test_check("safeinstall")
---
+++ tests/testthat/test-safe_install.R
@@ -0,0 +1,4 @@
+
+test_that("safe_install of testevil gives error", {
+  expect_error(safe_install_github("ropenscilabs/testevil"))
+})
