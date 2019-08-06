65e683c1b86356cbfd010cd712caa2178358a818
Pakillo <f.rodriguez.sanc@gmail.com>
2019-08-06 18:26:06 +0200

    add more tests

--- tests/testthat/test-safe_install.R
+++ tests/testthat/test-safe_install.R
@@ -2,3 +2,7 @@
 test_that("safe_install of testevil gives error", {
   expect_error(safe_install_github("ropenscilabs/testevil"))
 })
+
+test_that("safe_install of safeinstall does not give error (installs)", {
+  expect_true(is.character(safe_install_github("Pakillo/safeinstall")))
+})
